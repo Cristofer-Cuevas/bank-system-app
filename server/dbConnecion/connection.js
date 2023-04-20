1bba6dfce6bd358805f5399eae014b4a317bb072
programmerXS <programmerxs07@gmail.com>
2023-04-20 00:42:29 -0400

    g

--- server/dbConnecion/connection.js
+++ server/dbConnecion/connection.js
@@ -3,7 +3,7 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-    connectionString: "postgres://christopher:i6QKzqGbT51beU7R5qj94aJfgqAS7JQn@dpg-ch0aae3h4hsukp11b350-a/banksystem",
+    connectionString: "postgres://christopher:i6QKzqGbT51beU7R5qj94aJfgqAS7JQn@dpg-ch0aae3h4hsukp11b350-a/",
     user: "christopher",
     password: "i6QKzqGbT51beU7R5qj94aJfgqAS7JQn",
     host: "dpg-ch0aae3h4hsukp11b350-a",
